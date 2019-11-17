08bef737ecd7144b3901417137a7250f3ea3d9e4
tetsu5555 <tetete1118@gmail.com>
2019-11-17 14:30:13 +0900

    feat: add post entory point

--- api/server.js
+++ api/server.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose');
 var databaseUrl = process.env.MONGO_DATABASE || "mongodb://localhost/myapp"
 var Todo = require('./models').Todo;
 
+// reqest bodyをパースする
+app.use(express.json())
+
 mongoose.connect(databaseUrl, { useNewUrlParser: true });
 
-app.get('/api/todos', function(req, res) {
+app.get('/todos', function(req, res) {
     Todo.find().exec((err, todos) => {
         if (err) {
             res.send(err)
@@ -15,5 +18,20 @@ app.get('/api/todos', function(req, res) {
         res.json(todos)
     })
 });
+
+app.post('/todos', function(req, res) {
+    const todo = new Todo()
+    console.log(req)
+    console.log(req.body)
+
+    todo.title = req.body.title
+    todo.done = false
+    todo.save((err, doc) => {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(doc)
+    })
+})
 
 app.listen(8080);
