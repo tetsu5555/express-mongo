360796c6fa81260e9ada137082dd5bc2570a0f4c
tetsu5555 <tetete1118@gmail.com>
2019-12-26 13:46:26 +0900

    transaction

---
+++ mongoose-test/scripts/transaction.js
@@ -0,0 +1,139 @@
+// mongo
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.Promise = global.Promise
+// mongoose.set('debug', true) // enable logging collection methods + arguments to the console
+const databaseUrl = process.env.MONGO_DATABASE
+mongoose.connect(databaseUrl)
+
+
+const schema1 = new Schema({
+  name: {type: String},
+}, {
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+    transform: (doc, m) => {
+      delete m.__v
+      return m
+    },
+  },
+})
+
+const User = mongoose.model('User', schema1)
+
+const schema2 = new Schema({
+  msg: {type: String, required: true},
+}, {
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+    transform: (doc, m) => {
+      delete m.__v
+      return m
+    },
+  },
+})
+
+const Chat = mongoose.model('Chat', schema2)
+
+
+const insert = async function(isFail = false) {
+
+  // session取得(Mongooseの場合はModel.startSessionで取得)
+  const session = await User.startSession()
+
+
+  try {
+    console.log('aaaa')
+    await session.startTransaction()
+    console.log('aaaaaaaaa')
+
+    // テーブルがないと失敗するので注意
+    console.log('aaaaaaaaa isFail before', isFail)
+    await User.insertMany([{name: '次郎'}],  { session })
+    await Chat.insertMany([{msg: 'こんにちわ'}],  { session })
+    console.log('aaaaaaaaa isFail', isFail)
+    if (isFail) {
+      throw new Error('保存失敗')
+    }
+
+    console.log('aaaaaaaaa beforeCommit')
+    await session.commitTransaction() // コミット
+    console.log('--------commit-----------')
+  } catch (e) {
+    await session.abortTransaction() // ロールバック
+    console.log('--------abort-----------')
+    console.error(e)
+  } finally {
+    await session.endSession()
+  }
+
+  // 状態確認
+  const users = await User.find()
+  console.log(users)
+  const chats = await Chat.find()
+  console.log(chats)
+}
+
+
+const update = async function(isFail = false) {
+
+  // session取得(Mongooseの場合はModel.startSessionで取得)
+  const session = await User.startSession()
+  try {
+    await session.startTransaction({readConcern: { level: 'snapshot' }, writeConcern: { w: 'majority' }})
+
+    await User.findOneAndUpdate({name: '次郎'}, {$set: {name: '花子'}}, {session, runValidators: true, new: true})
+    await Chat.findOneAndUpdate({msg: 'こんにちわ'}, {$set: {msg: 'こんばんわ'}}, {session, runValidators: true, new: true})
+    if (isFail) {
+      throw new Error('保存失敗')
+    }
+
+    await session.commitTransaction() // コミット
+    console.log('--------commit-----------')
+  } catch (e) {
+    await session.abortTransaction() // ロールバック
+    console.log('--------abort-----------')
+    console.error(e)
+  } finally {
+    await session.endSession()
+  }
+
+  // 状態確認
+  const users = await User.find()
+  console.log(users)
+  const chats = await Chat.find()
+  console.log(chats)
+}
+
+
+
+async function main() {
+
+  await mongoose.connection.dropDatabase()
+
+  // テーブル作成
+  await User.create({name: '太郎'})
+  await Chat.create({msg: 'おはよう'})
+  // 状態確認
+  const users = await User.find()
+  console.log(users)
+  const chats = await Chat.find()
+  console.log(chats)
+
+//   await insert(true)
+  await insert()
+//   await update(true)
+//   await update()
+
+  await mongoose.disconnect()
+}
+
+main()
